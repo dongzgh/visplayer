b311bdfbadf360237b80ae99ed7c28e7160ca4c7
dzhao <dongz.vip@gmail.com>
2015-03-17 09:16:37 -0400

    Changed mongo.txt to mongo.sh

--- data/mongo.sh
+++ data/mongo.sh
@@ -1,11 +1,11 @@
---------------------------------------------------------------------------------------
-//  start mongodb
-----------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------
+#  start mongodb
+#----------------------------------------------------------------------------------------
 mongod --dbpath <db_path>
 mongod --dbpath "G:\Dong\My Documents\Private\Project\Technology\visPlayer\data\db"
 mongod --dbpath "C:\Users\dzhao\My Projects\General\visPlayer\data\db"
 mongod --dbpath <log_path>
-mongod --dbpath "G:\Dong\My Documents\Private\Project\Technology\visPlayer\data\log\mongod.log""
+mongod --dbpath "G:\Dong\My Documents\Private\Project\Technology\visPlayer\data\log\mongod.log"
 mongod --logpath "C:\Users\dzhao\My Projects\General\visPlayer\data\log\mongod.log"
 mongo
 
