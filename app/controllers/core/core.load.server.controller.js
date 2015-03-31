83d77c23b2ee91a9bf9d5ea51a6567e83001aad4
dongz <dongz.vip@gmail.com>
2015-03-31 17:27:44 -0400

    Removed encryption and decryption for loading parts

--- app/controllers/core/core.load.server.controller.js
+++ app/controllers/core/core.load.server.controller.js
@@ -37,24 +37,29 @@ exports.loadVis = function(res, filepath) {
           edges: edata
         };
 
-        // Encrypt gd
-        var salt = CryptoJS.lib.WordArray.random(128 / 8);
-        var key = CryptoJS.EvpKDF(res.req.user.id, salt, {
-          keySize: 128 / 32
-        });
-        var iv = CryptoJS.enc.Hex.parse('101112131415161718191a1b1c1d1e1f');
-        var enc = CryptoJS.AES.encrypt(JSON.stringify(gd), key, {
-          iv: iv,
-          mode: CryptoJS.mode.CBC
-        });
-        var raw = {
-          iv: CryptoJS.enc.Hex.stringify(iv),
-          salt: CryptoJS.enc.Hex.stringify(salt),
-          ciphertext: CryptoJS.enc.Hex.stringify(enc.ciphertext)
-        };
+        // // Encrypt gd
+        // var salt = CryptoJS.lib.WordArray.random(128 / 8);
+        // var key = CryptoJS.EvpKDF(res.req.user.id, salt, {
+        //   keySize: 128 / 32
+        // });
+        // var iv = CryptoJS.enc.Hex.parse('101112131415161718191a1b1c1d1e1f');
+        // var enc = CryptoJS.AES.encrypt(JSON.stringify(gd), key, {
+        //   iv: iv,
+        //   mode: CryptoJS.mode.CBC
+        // });
+        // var raw = {
+        //   iv: CryptoJS.enc.Hex.stringify(iv),
+        //   salt: CryptoJS.enc.Hex.stringify(salt),
+        //   ciphertext: CryptoJS.enc.Hex.stringify(enc.ciphertext)
+        // };
 
-        // Send data
-        var msg = JSON.stringify(raw);
+        // // Send encrypted data
+        // var msg = JSON.stringify(raw);
+        // res.set('ContentLength', msg.length);
+        // res.send(msg).status(200).end();
+
+        // Send unencrypted data
+        var msg = JSON.stringify(gd);
         res.set('ContentLength', msg.length);
         res.send(msg).status(200).end();
       });
