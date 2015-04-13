18130bbed8101b13816e4ff03522a08ab3c890d3
dongz <dongz.vip@gmail.com>
2015-04-13 17:44:12 -0400

    Added list for input

--- public/modules/core/controllers/upload-files.client.controller.js
+++ public/modules/core/controllers/upload-files.client.controller.js
@@ -2,8 +2,15 @@
 
 angular.module('slides').controller('UploadFilesController', ['$scope', '$log', '$upload', 'Files',
   function($scope, $log, $upload, Files) {
+    $scope.list = [];
+
     $scope.upload = function (files) {
       $log.log(files);
+      if(angular.isDefined(files) && files.length > 0) {
+        files.forEach(function(file){
+          $scope.list.push(file.name);
+        });
+      }        
     };
     
     // Watch on files
