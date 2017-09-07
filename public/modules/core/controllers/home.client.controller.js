6694f62f5d0594aeeb7e21f6b5c404d096b73211
dongz <dongz.vip@gmail.com>
2017-09-07 15:35:51 -0400

    Reorganized file locations

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 angular.module('core').controller('HomeController', ['$rootScope', '$scope', 'Authentication', 'Languages',
   function($rootScope, $scope, Authentication, Languages) {
     // This provides Authentication context.
