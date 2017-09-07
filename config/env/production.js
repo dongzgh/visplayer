6de45f87813dc5fa315d29731848ff480aa0f441
dongz <dongz.vip@gmail.com>
2017-09-07 16:36:42 -0400

    Updated production.js environment variable setting

--- config/env/production.js
+++ config/env/production.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   db: {
-		uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/visplayer',
+		uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/visplayer',
 		options: {
 			user: '',
 			pass: ''
