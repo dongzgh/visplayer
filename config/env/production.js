6d5cfa66fc32cb4da989b68ee40173bf77d52198
dongz <dongz.vip@gmail.com>
2017-09-07 22:53:12 -0400

    Updated mongodb location

--- config/env/production.js
+++ config/env/production.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   db: {
-		uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/visplayer',
+		uri: process.env.MONGODB_URI,
 		options: {
 			user: 'heroku_0rb2xq43',
 			pass: 'k46efblp5b4ldemrh3re11fqa8'
