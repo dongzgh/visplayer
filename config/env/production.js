895abe80c67649edff1c450d1da1b0cf58b9259b
dongz <dongz.vip@gmail.com>
2017-09-07 22:16:15 -0400

    Revised deployment settings

--- config/env/production.js
+++ config/env/production.js
@@ -4,8 +4,8 @@ module.exports = {
   db: {
 		uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/visplayer',
 		options: {
-			user: '',
-			pass: ''
+			user: 'heroku_0rb2xq43',
+			pass: 'k46efblp5b4ldemrh3re11fqa8'
 		}
 	},
 	log: {
