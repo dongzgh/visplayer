107bd0fb1548be93f9e02dd054d1a4919f4b6ded
dongz <dongz.vip@gmail.com>
2017-09-08 16:23:44 -0400

    Reorganized fonts place

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ var init = require('./config/init')(),
 // Bootstrap db connection
 var db = mongoose.connect(config.db.uri, config.db.options, function(err) {
 	if (err) {
+		console.log(config.db.uri);
 		console.error(chalk.red('Could not connect to MongoDB!'));
 		console.log(chalk.red(err));
 	}
