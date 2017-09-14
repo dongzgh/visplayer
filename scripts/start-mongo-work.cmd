22c2e12aedbb565038d8d240ae6a098d88c54cc4
dongz <dongz.vip@gmail.com>
2017-09-13 23:49:32 -0400

    change underscore to dash in project name

--- scripts/start-mongo-work.cmd
+++ scripts/start-mongo-work.cmd
@@ -1,2 +1,2 @@
-mongod --logpath "C:\Users\dzhao\Projects\General\visplayer_test\data\log\access.log"
-mongod --dbpath "C:\Users\dzhao\Projects\General\visplayer_test\data\db"
+mongod --logpath "C:\Users\dzhao\Projects\General\visplayer-test\data\log\access.log"
+mongod --dbpath "C:\Users\dzhao\Projects\General\visplayer-test\data\db"
