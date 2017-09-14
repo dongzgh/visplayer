0a0e23c30ce3f8cfbbba8d2aabe055c44e99f5f9
dongz <dongz.vip@gmail.com>
2017-09-13 23:57:06 -0400

    revert script directory settings

--- scripts/start-mongo-home.cmd
+++ scripts/start-mongo-home.cmd
@@ -1,2 +1,2 @@
-mongod --logpath "G:\Dong\My Projects\visplayer-test\data\log\access.log"
-mongod --dbpath "G:\Dong\My Projects\visplayer-test\data\db"
+mongod --logpath "G:\Dong\My Projects\visplayer\data\log\access.log"
+mongod --dbpath "G:\Dong\My Projects\visplayer\data\db"
