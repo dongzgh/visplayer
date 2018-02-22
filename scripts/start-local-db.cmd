716a55101811f730fb5dc75547e87bd2efe0e604
dongz <dongz.vip@gmail.com>
2018-02-21 23:45:52 -0500

    updated start-local-db.cmd

--- scripts/start-local-db.cmd
+++ scripts/start-local-db.cmd
@@ -1,2 +1,2 @@
-mongod --logpath "C:\Users\dzhao\Projects\General\visplayer\data\log\access.log"
-mongod --dbpath  "C:\Users\dzhao\Projects\General\visplayer\data\db"
+mongod --logpath "..\data\log\access.log"
+mongod --dbpath  "..\data\db"
